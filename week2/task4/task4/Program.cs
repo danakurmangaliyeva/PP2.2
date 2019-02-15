1e9ab8381b846576b26e6a3171e7a33499e9c26f
danakurmangaliyeva <dako_asalim@mail.ru>
2019-02-16 01:55:08 +0600

    vf

--- week2/task4/task4/Program.cs
+++ week2/task4/task4/Program.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 using System.IO;
 
-
 namespace task4
 {
     class Program
     {
-
-        public static void CopyDelete(string path,string path 1)
+        static void CopDel(string path,string path1)//функция 
         {
-
-
-
+            string comb = Path.Combine(path, "text.txt");//строка комбинирущая путь и название файла
+            string comb1 = Path.Combine(path1, "test.txt"); //строка комбинирущая путь и название файла
+            StreamWriter sv = new StreamWriter(comb);//открываем поток для первого файла
+            sv.Write("fnfn");//пишем туда и закрываем
+            sv.Close();
+            File.Copy(comb, comb1);//копируем из одного в другой
+            File.Delete(comb);//удаляем исходник
         }
-
+   
         static void Main(string[] args)
         {
-
-        } 
+            CopDel(@"null", @"nulll");//указываем два пути
+        }
     }
 }
